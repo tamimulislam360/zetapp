55fa5db2652065b658b18d8726b5604b23eb4f52
Tamimul Islam <hmtamim24@gmail.com>
2023-06-21 19:52:15 +0600

    sliders added

--- components/Earnings/Earnings.tsx
+++ components/Earnings/Earnings.tsx
@@ -7,7 +7,24 @@ import Image from 'next/image';
 // import video from '../../public/assets/videos/Step3.mp4'
 
 
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+
 const Earnings = () => {
+    const steps = [
+        'Download the App and Sign up as a ZET agent',
+        'Register your customers and Recommend financial products',
+        'Start earning upto ₹ 1 Lakh every month'
+    ]
     return (
         <div data-aos="fade-right" data-aos-duration="1000" className={`bg-[#0A2540] text-white ${styles.clip} py-44`}>
             <div className="max-w-screen-xl max-auto p-6 flex flex-col items-center gap-6">
@@ -15,20 +32,38 @@ const Earnings = () => {
                     header='Start Earning in 3 Easy Steps'
                     subheader='We have create the app the make your earning easy'
                 />
-                <div className="flex flex-col md:flex-row md:justify-between gap-3 justify-center items-center">
-                    <div className='flex flex-col gap-3 items-center md:items-start'>
-                        <Typography variant='h5'>Step 3</Typography>
-                        <Typography variant='h3' className="text-center md:text-left">Start earning upto ₹ 1 Lakh every month</Typography>
+                <Swiper
+                    modules={[A11y, Autoplay, Pagination]}
+                    pagination
 
-                        <Image src={googlePlay} alt="" />
-                    </div>
-                    <div className='max-w-[200px]'>
-                    <video className="h-full w-full rounded-lg" controls autoPlay>
-                        <source src="/Step3.mp4" type="video/mp4" />
-                        Your browser does not support the video tag.
-                        </video>
-                    </div>
-                </div>
+                    autoplay={
+                        {
+                            delay: 2000,
+                            disableOnInteraction: false,
+                            // stopOnLastSlide: false
+                        }
+                    }
+                    loop={true}
+                    className="flex flex-col md:flex-row md:justify-between gap-3 justify-center items-center">
+                    {
+                        steps.map((st, i) => (
+                            <SwiperSlide key={i} className="flex flex-col md:flex-row gap-3 items-center md:items-start">
+                                <div className='flex flex-col gap-3 items-center md:items-start'>
+                                    <Typography variant='h5'>Step {i + 1}</Typography>
+                                    <Typography variant='h3' className="text-center md:text-left">{st}</Typography>
+
+                                    <Image src={googlePlay} alt="" />
+                                </div>
+                                <div className='max-w-[200px]'>
+                                <video className="h-full w-full rounded-lg" controls autoPlay>
+                                    <source src="/Step3.mp4" type="video/mp4" />
+                                    Your browser does not support the video tag.
+                                    </video>
+                                </div>                                              
+                            </SwiperSlide>
+                        ))
+                    }
+                </Swiper>
             </div>
         </div>
     );
--- components/people/People.tsx
+++ components/people/People.tsx
@@ -3,8 +3,23 @@ import {people} from '@/components/images'
 import Image from 'next/image';
 import { Typography } from '@/components/materialExport';
 import styles from './People.module.css'
+// import Swiper core and required modules
+import { Navigation, Pagination, Scrollbar, A11y, Autoplay } from 'swiper';
+
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const People = () => {
+    const contents = [
+        '14 Lakh+ Financial Advisor across India are using ZET to increase their income.',
+        'We helped customers in more than 50 cities to get their first financial product',
+        'More than ₹20 Cr earned by our Agents by selling Credit Cards and Loans'
+    ]
     return (
         <div data-aos="fade-in" data-aos-duration="1000" className={`${styles.clip} my-16 scroll-m-0 bg-primary px-4 py-12 text-white `}>
             <div className={`${styles.clip} max-w-screen-xl mx-auto flex justify-between items-center gap-2 bg-[#417FDA] rounded p-1 relative h-full`}>
@@ -15,9 +30,28 @@ const People = () => {
                     <Typography variant="h5" className="mb-4">
                         THE ZET EFFECT
                     </Typography>
-                    <Typography variant="h3">
-                    14 Lakh+ Financial Advisor across India are using ZET to increase their income.
-                    </Typography>
+                    <Swiper
+                        modules={[A11y, Autoplay, Pagination]}
+                        pagination
+                        autoplay={
+                            {
+                                delay: 2000,
+                                disableOnInteraction: false,
+                                // stopOnLastSlide: false
+                            }
+                        }
+                        loop={true}
+                    >
+                        {
+                            contents.map((content, i) => (
+                                <SwiperSlide key={i}>
+                                    <Typography variant="h3">
+                                        {content}
+                                    </Typography>
+                                </SwiperSlide>
+                            ))
+                        }
+                    </Swiper>
                 </div>
             </div>
         </div>
